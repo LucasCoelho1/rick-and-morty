92361764e17afd89c67b95558d1656b25b3f9dd8
LucasCoelho1 <lucas.coelho3@alunos.sc.senac.br>
2023-03-20 14:55:49 -0300

    Asasa

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './css/App';
-import reportWebVitals from './report/reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/index.css";
+import App from "./App";
+import reportWebVitals from "./report/reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
